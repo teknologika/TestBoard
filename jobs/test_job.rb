37d277cf5bbcb3f117791247e8c22e213cb7b01e
Bruce McLeod <bruce@Yager.local>
2015-02-11 14:51:29 +1100

    Test widget can now scale

--- jobs/test_job.rb
+++ jobs/test_job.rb
@@ -2,8 +2,8 @@ require 'nokogiri'
 
  # Any junit soource file will need to be added to the sources list
  sources = {
-    'system_test_data' => "http://localhost/system_tests.xml",
-    'unit_test_data' => "http://localhost/unit_tests.xml"
+    'system_test_data' => "http://localhost:3030/assets/unit_tests.xml",
+    'unit_test_data' => "http://localhost:3030/assets/unit_tests.xml"
   }
 
 
@@ -31,26 +31,26 @@ SCHEDULER.every '10s', :first_in => 0 do |job|
       puts "\e[33mError reading Junit file: " + url + " \e[0m"
     end
 
-    if @doc 
+    if @doc
       duration =  @doc.at_css("testsuite").attr("time").to_i
       ignored = @doc.at_css("testsuite").attr("skipped").to_i
       if ignored > 0
         status = "danger"
       end
-  
-      failed_tests = @doc.at_css("testsuite").attr("failures").to_i 
+
+      failed_tests = @doc.at_css("testsuite").attr("failures").to_i
       if failed_tests > 0
-        status = "warning" 
+        status = "warning"
       end
 
       total_tests = @doc.at_css("testsuite").attr("tests").to_i
       passing = total_tests.to_i - failed_tests - ignored
-      
+
     end
 
     # Note: not run is hardcodded to zero. Setting Not run greater than 0 will introcude a black gap into the chart
     send_event(source, { passed: passing, last_passed: last_passing[source],ignored: ignored, failed: failed_tests, not_run:0, duration:duration, status:status})
-  
+
     last_passing[source] = passing
   end
 end
